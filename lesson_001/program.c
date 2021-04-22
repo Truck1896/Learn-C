9cc7e49cb74afc2ff7b114e983de6ac77634f4f0
Truck1896 <83038094+Truck1896@users.noreply.github.com>
2021-04-22 22:28:47 +0200

    Create program.c

---
+++ lesson_001/program.c
@@ -0,0 +1,26 @@
+/*
+  If you encounter any errors, feel free to create an issue and describe your error. I will do my best
+  to help you.
+*/
+
+#include <stdio.h>
+
+/*
+  This is a comment. It will be ignored by the compiler and is used to describe code. 
+  C files should have a '.c' extension.
+  All C programs have a main function. This function will be executed when you run the program.
+*/
+int main()
+{
+  /*
+    printf() is a function used to output code onto the console. In this example, we will
+    only make a 'Hello World!' message, which is a good way to start out.
+  */
+  printf("Hello World!");
+  
+  /*
+    By saying 'return 0;', we say that the program ran successfully without errors. If your program 
+    has an error, it will not return 0.
+  */
+  return 0;
+}
